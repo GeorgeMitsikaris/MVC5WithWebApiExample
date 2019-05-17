8b20c690b4cb81f75acd888797d4f06975e89114
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-17 15:37:00 +0300

    Add project files.

---
+++ MVC5WithWebApiExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5WithWebApiExample.Global" Language="C#" %>
---
+++ MVC5WithWebApiExample/Models/ItemType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5WithWebApiExample.Models
+{
+    public class ItemType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(5)]
+        [RegularExpression(@"\d+")]
+        [Required]
+        public int Code { get; set; }
+
+        [MaxLength(30)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
