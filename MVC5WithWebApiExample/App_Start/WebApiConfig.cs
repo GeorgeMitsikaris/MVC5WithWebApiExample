649a7a1050726a29b4357e62b7fba6dcaa9b66b7
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-18 15:29:00 +0300

    Adding Itemtypes controller and creating Get, search and insert functionality

--- MVC5WithWebApiExample/App_Start/WebApiConfig.cs
+++ MVC5WithWebApiExample/App_Start/WebApiConfig.cs
@@ -14,11 +14,13 @@ namespace MVC5WithWebApiExample
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
         }
     }
 }
---
+++ MVC5WithWebApiExample/Controllers/WebApi/ItemTypesController.cs
@@ -0,0 +1,56 @@
+﻿using MVC5WithWebApiExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVC5WithWebApiExample.Controllers.WebApi
+{
+    [RoutePrefix("itemtypes")]
+    public class ItemTypesController : ApiController
+    {
+        ItemTypeDBContext _db = new ItemTypeDBContext();
+
+        // GET: api/ItemTypes
+        [Route("")]
+        public IEnumerable<ItemType> Get()
+        {
+            var itemTypes = _db.ItemTypes.ToList();
+            return itemTypes;
+        }
+
+        // GET: api/ItemTypes/5
+        [Route("search")]
+        public ItemType Get(string search)
+        {
+            var itemTypeName = _db.ItemTypes.FirstOrDefault(i => i.Name.Contains(search));
+            return itemTypeName;
+        }
+
+        // POST: api/ItemTypes
+        [Route("")]
+        public void Post([FromBody]ItemType itemType)
+        {
+            if (itemType == null)
+                throw new NullReferenceException("You must enter a value");
+            else
+            {
+                _db.ItemTypes.Add(itemType);
+                _db.SaveChanges();
+            }
+        }
+
+        // PUT: api/ItemTypes/5
+        [Route()]
+        public void Put(int id, [FromBody]ItemType itemType)
+        {
+        }
+
+        // DELETE: api/ItemTypes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
